5550b41925728707a1f07da6a3a6805eaee6ec5e
Carlos Gomez <cgomezlugo@hotmail.com>
2023-12-17 11:56:26 -0400

    features/models set user and card models

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    validate: {
+      validator(v) {
+        return /^(https?:\/\/)(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/.test(
+          v
+        );
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, 'User phone number required'],
+  },
+});
+
+const user = mongoose.model('user', userSchema);
+module.exports = user;
