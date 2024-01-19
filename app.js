582f5d7af3bbdb4d8fe3251caf91a06215cdfa96
Carlos Gomez <cgomezlugo@hotmail.com>
2024-01-19 11:15:46 -0400

    monngoose.conect changes

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect('mongodb://localhost:27017/aroundb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Failed to connect to MongoDB', err));
